11d3c5d42049f76cae89b424862566af3a88088e
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-18 17:16:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
   (2...num)
   if num == 2
     return true 
-  elsif num.abs % 2 == 0 || num < 0 
+  elsif num.abs % 2 == 0 || num < 2 
     return false 
   else return true 
   end 
