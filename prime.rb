326e21fb9edd66b36ecfeed189b0780b980477f7
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-18 17:25:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(num)
   if num <= 1 
     return false
   else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+    (2..num-1).to_a.all? do |divisor|
+      num % divisor != 0
     end
   end
 end
